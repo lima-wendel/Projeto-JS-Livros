a4373756296857d989479a95eb424fbec4aacc1c
lima-wendel <wchristofer@gmail.com>
2021-07-07 09:55:48 -0300

    atualizando a biblioteca

--- database.js
+++ database.js
@@ -8,4 +8,33 @@ const livros = [
         recomenda: false,
         leu: false
     },
+    {
+        id: 2,
+        nome: "O Desejado de Todas as Nações",
+        autor: "Ellen G White",
+        categoria: "Religião",
+        paginas: 600,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome: "JavaScript Guia do Programador",
+        autor: "Mauricio Samy Silva",
+        categoria: "Tecnologia",
+        paginas: 580,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 4,
+        nome: "ALgoritmos para viver",
+        autor: "Brian Christian",
+        categoria: "Tecnologia",
+        paginas: 412,
+        recomenda: true,
+        leu: true
+    }
 ]
+// o nodeJS peermite a exportação desses dados
+module.exports = livros
