ca0883580a1d624cb032c4a6daf5bc0bc038d4b8
lima-wendel <wchristofer@gmail.com>
2021-07-07 09:45:25 -0300

    Iniciando projeto

---
+++ database.js
@@ -0,0 +1,11 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Digital Minimalism",
+        autor: "Cal Newport",
+        categoria: "Produtividade e estilo de vida",
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+]
