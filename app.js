468982d2178239414c5804a82f034d87df882341
lima-wendel <wchristofer@gmail.com>
2021-07-07 10:09:07 -0300

    configuração inicial da importação do readline

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 // importando a biblioteca
 const livros = require('./database')
-console.log(livros)
+
+
+// input: escolhendo o livro por categoria
+const readline = require('readline-sync')
+const entradaInicial = readline.question('Deseja buscar um livro? S/N')
+
+//se sim, exibir categorias disponíveis e perguntar qual categoria deseja escolher
+
+
+
+// se não, exibir todos os livros em ordem crescente pela quantidade de páginas
