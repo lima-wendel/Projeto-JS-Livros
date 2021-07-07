989735f56eae69f3df3f5d0c05d02d10e9d1c987
lima-wendel <wchristofer@gmail.com>
2021-07-07 10:27:47 -0300

    retornando toda a lista de livros

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const readline = require('readline-sync')
 const entradaInicial = readline.question('Deseja buscar um livro? S/N')
 if (entradaInicial.toLocaleUpperCase() === 'S') {
     console.log('Essas são as categorias disponíveis:')
-    console.log('Produtividade e estilo de vida', '/ Religião', '/ Tecnologia')
+    console.log('Produtividade', '/ Religião', '/ Tecnologia')
 
     const entradaCategoria = readline.question('Qual categoria você escolhe?')
     const retorno = livros.filter(livro => livro.categoria === entradaCategoria) 
     console.table(retorno)
+} else {
+    const livrosOrdenados = livros.sort((a, b) => a.paginas - b.paginas)
+    console.log('Essas são todas os livros disponíveis:')
+    console.table(livrosOrdenados)
 }
 
