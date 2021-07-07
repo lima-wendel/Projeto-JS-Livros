d15575760c6a3605cf22166108e5439c5ec108ef
lima-wendel <wchristofer@gmail.com>
2021-07-07 10:23:33 -0300

    criando input para escolha por categoria

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // importando a biblioteca
 const livros = require('./database')
 
-
 // input: escolhendo o livro por categoria
+//se sim, exibir categorias disponíveis e perguntar qual categoria deseja escolher
+// se não, exibir todos os livros em ordem crescente pela quantidade de páginas
+
 const readline = require('readline-sync')
 const entradaInicial = readline.question('Deseja buscar um livro? S/N')
+if (entradaInicial.toLocaleUpperCase() === 'S') {
+    console.log('Essas são as categorias disponíveis:')
+    console.log('Produtividade e estilo de vida', '/ Religião', '/ Tecnologia')
 
-//se sim, exibir categorias disponíveis e perguntar qual categoria deseja escolher
+    const entradaCategoria = readline.question('Qual categoria você escolhe?')
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria) 
+    console.table(retorno)
+}
 
-
-
-// se não, exibir todos os livros em ordem crescente pela quantidade de páginas
